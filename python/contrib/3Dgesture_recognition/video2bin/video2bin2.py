ec9116b97c226390fd92e23c44590ff15d1faa97
JiangFeng11 <45545092@qq.com>
2021-04-10 21:04:16 +0800

    add video2bin

--- python/contrib/3Dgesture_recognition/video2bin/video2bin2.py
+++ python/contrib/3Dgesture_recognition/video2bin/video2bin2.py
@@ -1,4 +1,6 @@
-## Video to bin new 
+"""
+ Video to bin new 
+"""
 
 import cv2
 import numpy as np
@@ -8,25 +10,28 @@ import os
 
 FILEPATH = './test_actiontype7/'
 OUTFILE = './Output/float/test_float32_actiontype7.bin'
-#OUTFILE = './Output/float32/test_actiontype1.bin'
 
 CROP_SIZE = 112
 CHANNEL_NUM = 3
 CLIP_LENGTH = 16
 
 def file_names():
+"""
+ read file names
+"""
     F = []
     for root, dirs, files in os.walk(FILEPATH):
-        for file in files:
-            #print file.decode('gbk')  
+        for file in files:  
             if os.path.splitext(file)[1] == '.jpg':
-                #print(root + file) 
                 F.append(root + file) 
     return F
 
 
 def read_images(imglist):
-    imgArray = np.empty([1,CLIP_LENGTH, CROP_SIZE, CROP_SIZE ,CHANNEL_NUM], dtype = np.float32)
+"""
+ read images
+"""
+    imgArray = np.empty([1, CLIP_LENGTH, CROP_SIZE, CROP_SIZE, CHANNEL_NUM], dtype = np.float32)
     i = 0
     for img in imglist:
         image = cv2.imread(img)
@@ -34,19 +39,11 @@ def read_images(imglist):
         imgArray[0][i] = image 
         i = i + 1
         print(imgArray[0][i-1])
-        #print(image.shape)
 
     return imgArray
 
 
 imageNames = file_names()
 imageNames.sort()
-
-print(imageNames)
-
 imgArray = read_images(imageNames)
-print(imgArray.shape)
-
-print(imgArray)
-
 imgArray.tofile(OUTFILE)
