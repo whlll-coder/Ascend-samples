a6131f364c3f621ab3a5425fd233c35370809b3c
caoliang <leoncao61@163.com>
2021-04-14 18:19:37 +0800

    update cplusplus/contrib/TextRecognize/scripts/testcase_200dk.sh.

--- cplusplus/contrib/TextRecognize/scripts/testcase_200dk.sh
+++ cplusplus/contrib/TextRecognize/scripts/testcase_200dk.sh
@@ -231,25 +231,23 @@ function main() {
     if [[ ${project_pid}"X" != "X" ]];then
         echo -e "\033[33m kill existing project process: kill -9 ${project_pid}.\033[0m"
         sudo kill -9 ${project_pid}
-        return ${success}
-        #if [ $? -ne 0 ];then
-            #echo "ERROR: kill project process failed."
-            #return ${inferenceError}
-        #fi
+        if [ $? -ne 0 ];then
+            echo "ERROR: kill project process failed."
+            return ${inferenceError}
+        fi
 
         presenter_server_pid=`ps -ef | grep "presenter_server\.py" | awk -F ' ' '{print $2}'`
         if [[ ${presenter_server_pid}"X" != "X" ]];then
             echo -e "\033[33mNow do presenter server configuration, kill existing presenter process: kill -9 ${presenter_server_pid}.\033[0m"
             sudo kill -9 ${presenter_server_pid}
-            return ${success}
-            #if [ $? -ne 0 ];then
-                #echo "ERROR: kill presenter server process failed."
-                #return ${inferenceError}
-            #fi
+            if [ $? -ne 0 ];then
+                echo "ERROR: kill presenter server process failed."
+                return ${inferenceError}
+            fi
         fi
-    #else 
-        #echo "ERROR: run failed. please check your project"
-        #return ${inferenceError}
+    else 
+        echo "ERROR: run failed. please check your project"
+        return ${inferenceError}
     fi
 
     echo "run success"
