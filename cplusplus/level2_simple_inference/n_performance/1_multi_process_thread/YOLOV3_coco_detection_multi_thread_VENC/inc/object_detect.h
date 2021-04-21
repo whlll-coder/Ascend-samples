f47b2a6179bab0a8efdf08626739e87f04f74e52
yy0131 <1595992831@qq.com>
2021-04-21 09:35:08 +0800

    modified

--- cplusplus/level2_simple_inference/n_performance/1_multi_process_thread/YOLOV3_coco_detection_multi_thread_VENC/inc/object_detect.h
+++ cplusplus/level2_simple_inference/n_performance/1_multi_process_thread/YOLOV3_coco_detection_multi_thread_VENC/inc/object_detect.h
@@ -18,15 +18,13 @@
 */
 #pragma once
 
+#include <memory>
+#include "acl/acl.h"
+#include "atlasutil/atlas_model.h"
+#include "atlasutil/atlas_utils.h"
 #include <opencv2/opencv.hpp>
 #include "opencv2/imgcodecs/legacy/constants_c.h"
 #include "opencv2/imgproc/types_c.h"
-
-#include "utils.h"
-#include "acl/acl.h"
-#include "model_process.h"
-#include "dvpp_process.h"
-#include <memory>
 
 using namespace std;
 
@@ -37,42 +35,19 @@ class ObjectDetect {
     public:
     ObjectDetect(const char* modelPath, uint32_t modelWidth,
     uint32_t modelHeight);
+
     ~ObjectDetect();
-    //Inference initialization
-    Result Init();
-    /*
-    //Inference frame image preprocessing
-    Result Preprocess(cv::Mat& frame);
-    */
-    //Inference frame picture
-    Result Inference(aclmdlDataset*& inferenceOutput, cv::Mat& reiszeMat);
-    /*
-    //Inference output post-processing
-    Result Postprocess(cv::Mat& frame, aclmdlDataset* modelOutput);
-    */
-    void DestroyDvpp();
+
+    AtlasError Init();
+
+    AtlasError Inference(std::vector<InferenceOutput>& inferenceOutput, cv::Mat& reiszeMat);
+
     void DestroyResource();
 
     private:
-    //Initializes the ACL resource
-    Result InitResource();
-    //Loading reasoning model
-    Result InitModel(const char* omModelPath);
-    Result CreateModelInputdDataset();
-    /*
-    //Get data from model inference output aclmdlDataset to local
-    void* GetInferenceOutputItem(uint32_t& itemDataSize,
-    aclmdlDataset* inferenceOutput, uint32_t idx);
-    Result DrowBoundBoxToImage(vector<BBox>& detectionResults, cv::Mat& origImg);
-    //Release the requested resources
-    //void DestroyResource();
-    */
-    int32_t deviceId_;          //Device ID, default is 0
-    aclrtContext context_;      //add
-    aclrtStream stream_;        //add
-    //DvppProcess dvpp_;          //add
-    ModelProcess model_;        //Inference model instance
+    AtlasError CreateInput();
 
+    AtlasModel model_;          //Inference model instance
     const char* modelPath_;     //Offline model file path
     uint32_t modelWidth_;       //The input width required by the model
     uint32_t modelHeight_;      //The model requires high input
@@ -81,6 +56,5 @@ class ObjectDetect {
     uint32_t imageInfoSize_;
     void*    imageInfoBuf_;
     aclrtRunMode runMode_;      //Run mode, which is whether the current application is running on atlas200DK or AI1
-    //pthread_t threadId_;
     bool isInited_;             //Initializes the tag to prevent inference instances from being initialized multiple times
 };
