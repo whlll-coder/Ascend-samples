c97bc09c8947349eb9aef28e603dc71e76b4749c
xyttaotao <15150667798@163.com>
2021-05-10 10:11:59 +0000

    fix bug

--- python/level2_simple_inference/2_object_detection/YOLOV3_mask_detection_video/scripts/testcase_200dk.sh
+++ python/level2_simple_inference/2_object_detection/YOLOV3_mask_detection_video/scripts/testcase_200dk.sh
@@ -4,7 +4,7 @@ aipp_cfg="https://modelzoo-train-atc.obs.cn-north-4.myhuaweicloud.com/003_Atc_Mo
 model_name="yolo3_resnet18_yuv"
 presenter_server_name="mask_detection_video"
 
-data_source="https://c7xcode.obs.cn-north-4.myhuaweicloud.com/models/mask_detection_video/mask-true.mp4"
+data_source="https://c7xcode.obs.cn-north-4.myhuaweicloud.com/models/mask_detection_video/mask-true.h264"
 project_name="YOLOV3_mask_detection_video"
 
 version=$1
@@ -22,9 +22,9 @@ function downloadData() {
 
     mkdir -p ${project_path}/data/
 
-    wget -O ${project_path}/data/"mask-true.mp4"  ${data_source}  --no-check-certificate
+    wget -O ${project_path}/data/"mask-true.h264"  ${data_source}  --no-check-certificate
     if [ $? -ne 0 ];then
-        echo "download ture.mp4 failed, please check Network."
+        echo "download ture.h264 failed, please check Network."
         return 1
     fi
 
@@ -162,9 +162,10 @@ function main() {
     
     cd ${project_path}/src
     #python3.6 mask_detect.py
-    python3.6 main.py ../data/mask-true.mp4 &
-    
+    python3.6 main.py ../data/mask-true.h264 &
+
     sleep 8
+    
     echo ${project_name}    
         project_pid=`ps -ef | grep "main.py" | grep "data" | awk -F ' ' '{print $2}'`
     if [[ ${project_pid}"X" != "X" ]];then
