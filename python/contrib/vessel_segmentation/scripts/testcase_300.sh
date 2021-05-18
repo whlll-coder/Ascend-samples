0105befb5ba6e5e5187a687abcc7d12400b93b2a
JiangFeng11 <45545092@qq.com>
2021-05-18 11:32:47 +0800

    add vessel segmentation

--- python/contrib/vessel_segmentation/scripts/testcase_300.sh
+++ python/contrib/vessel_segmentation/scripts/testcase_300.sh
@@ -132,7 +132,7 @@ function main() {
 
         # convert model     
         cd ${project_path}/model/
-        atc --model=${project_path}/model/${caffe_prototxt##*/} --weight=${project_path}/model/${caffe_model##*/}  --framework=0 --output=${HOME}/models/${project_name}/${model_name} --input_format=NCHW --input_fp16_nodes=data -output_type=FP32 
+        atc --model=${project_path}/model/${caffe_prototxt##*/} --weight=${project_path}/model/${caffe_model##*/}  --framework=0 --output=${HOME}/models/${project_name}/${model_name} --soc_version=Ascend310 --input_format=NCHW --input_fp16_nodes=data -output_type=FP32 
         if [ $? -ne 0 ];then
             echo "ERROR: convert model failed"
             return ${inferenceError}
