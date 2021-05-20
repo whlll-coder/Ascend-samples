6482e945108f7f7410ba721e2304170a7d7c0743
JiangFeng11 <45545092@qq.com>
2021-05-20 15:50:45 +0800

    add lenet

--- python/level2_simple_inference/1_classification/lenet_mindspore_picture/scripts/convert.py
+++ python/level2_simple_inference/1_classification/lenet_mindspore_picture/scripts/convert.py
@@ -10,5 +10,5 @@ import numpy as np
 from mindspore import Tensor
 network = LeNet5()
 load_checkpoint("./checkpoint_lenet-1_1875.ckpt", network)
-input = np.random.uniform(0.0, 1.0, size = [1, 1, 32, 32]).astype(np.float32)
-export(network, Tensor(input), file_name = './mnist', file_format = 'AIR') 
+input_data = np.random.uniform(0.0, 1.0, size = [1, 1, 32, 32]).astype(np.float32)
+export(network, Tensor(input_data), file_name = './mnist', file_format = 'AIR') 
