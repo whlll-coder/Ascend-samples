8255d5c1511f19836e7fad587a35f1f25df06766
wjm <wangjiming5@huawei.com>
2021-06-09 08:31:17 +0800

    add sample

---
+++ cplusplus/level1_single_api/4_op_dev/1_custom_op/framework/onnx_plugin/leaky_relu_plugin.cc
@@ -0,0 +1,45 @@
+/* Copyright (C) 2019. Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the Apache License Version 2.0.You may not use this file except in compliance with the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Apache License for more details at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#include "graph/operator.h"
+#include "register/register.h"
+#include "json.hpp"
+
+using namespace ge;
+
+namespace domi {
+Status ParseParamsLeakyRelu(const ge::Operator& op_src, ge::Operator& op_dest) {
+  // trans op_src to op_dest
+  // if op_src get required attr failed, need to return Failed
+  // if op_src get optional attr failed, need to return Failed or set a default value
+  float negative_slope = 0.01f;
+  string attrs_string;
+  if (ge::GRAPH_SUCCESS == op_src.GetAttr("attribute", attrs_string)) {
+    json attrs = json::parse(attrs_string);
+    for (json attr : attrs["attribute"]) {
+      if (attr["name"] == "alpha" && attr["type"] == 1) {
+        negative_slope = attr["f"];
+      }
+    }
+  }
+
+  op_dest.SetAttr("negative_slope", negative_slope);
+  return SUCCESS;
+}
+
+REGISTER_CUSTOM_OP("LeakyReluDemo")
+    .FrameworkType(ONNX)  // type: CAFFE, TENSORFLOW, ONNX
+    .OriginOpType({"ai.onnx::8::LeakyRelu", "ai.onnx::9::LeakyRelu", "ai.onnx::10::LeakyRelu"})  // name in onnx module
+    .ParseParamsByOperatorFn(
+        ParseParamsLeakyRelu)  // AutoMappingFn for Tensorflow, ParseParamsFn need to realize for onnx
+    .ImplyType(ImplyType::TVM);
+}  // namespace domi
