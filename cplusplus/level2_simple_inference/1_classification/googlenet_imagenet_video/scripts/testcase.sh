ddc32f6c600de60ff0761bd714e9fe0bc2b3e8f5
wuyan <1136014326@qq.com>
2021-07-20 09:02:23 +0000

    update testcase.sh.

--- cplusplus/level2_simple_inference/1_classification/googlenet_imagenet_video/scripts/testcase.sh
+++ cplusplus/level2_simple_inference/1_classification/googlenet_imagenet_video/scripts/testcase.sh
@@ -13,7 +13,7 @@ version=$1
 
 script_path="$( cd "$(dirname $BASH_SOURCE)" ; pwd -P)"
 project_path=${script_path}/..
-
+common_script_dir=${script_path}/../../../../../common/
 run_command="./${project_name} ${project_path}/data/${data_name}"
 model_atc="atc --model=${project_path}/model/${caffe_prototxt##*/} --weight=${project_path}/model/${caffe_model##*/} --framework=0 --output=${HOME}/models/${project_name}/${model_name} --soc_version=Ascend310 --insert_op_conf=${project_path}/model/${aipp_cfg##*/} --input_shape="data:1,3,224,224" --input_format=NCHW"
 
@@ -23,8 +23,8 @@ declare -i inferenceError=1
 declare -i verifyResError=2
 
 
-
-. ${script_path}/../../../../../common/testcase_common.sh
+. ${common_script_dir}/testcase_common.sh
+#. ${script_path}/../../../../../common/testcase_common.sh
 
 function main() {
 
